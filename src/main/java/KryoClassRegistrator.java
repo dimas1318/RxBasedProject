f08966c5b00129bddb865eb5904525981d623784
Dmitry Parshin <parshin@phystech.edu>
2019-05-09 22:15:32 +0300

    removed unused code

--- src/main/java/KryoClassRegistrator.java
+++
@@ -1,7 +0,0 @@
-import com.esotericsoftware.kryo.Kryo;
-
-public interface KryoClassRegistrator {
-
-    void register(Kryo kryo);
-
-}
--- src/main/java/KryoContext.java
+++
@@ -1,8 +0,0 @@
-public interface KryoContext {
-
-    byte[] serialze(Object obj);
-
-    byte[] serialze(Object obj, int bufferSize);
-
-    Object deserialze(Class clazz, byte[] serialized);
-}
